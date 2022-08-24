fa9eedd288fc18667d646a3310faa06fe4d0aca7
ChadScarborough <chad.d.scarborough@gmail.com>
2022-08-24 15:39:47 -0700

    Add project files.

---
+++ RecipeBookCompiler/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace RecipeBookCompiler
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                string? line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                    break;
+
+                if (line.ToUpper() == "PRINT TWO PLUS TWO.")
+                    Console.WriteLine("FOUR");
+            }
+        }
+    }
+}
